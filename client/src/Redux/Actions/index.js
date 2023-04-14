455769c8e8707bcd5deff0696153c50120b62148
mrotvar10 <mrtovar10@gmail.com>
2023-04-14 18:30:13 -0400

    update 2

--- client/src/Redux/Actions/index.js
+++ client/src/Redux/Actions/index.js
@@ -10,16 +10,24 @@ import baseURL from '../../index'
 
 // Creamos el Action creator para primerosPokemons el cual me trae el arreglo de
 // mis 40 pokemons de la API + los de mi DB
-export function getPokemons () {
-    return function (dispatch){
-        return fetch(`${baseURL}/pokemons`)
-        .then(data => data.json())
-        .then(
-            pokemons => dispatch({type: DISPLAY_POKEMONS,
-            payload: pokemons})
-        )
-    }
+// export function getPokemons () {
+//     return function (dispatch){
+//         return fetch(`${baseURL}/pokemons`)
+//         .then(data => data.json())
+//         .then(
+//             pokemons => dispatch({type: DISPLAY_POKEMONS,
+//             payload: pokemons})
+//         )
+//     }
+// }
+
+export function getPokemons() {
+    return dispatch => axios('/pokemons')
+    .then (res=>dispatch({type:DISPLAY_POKEMONS,payload: res.data}))
+    .catch( err => console.log(err))
 }
+
+
         
 export function postPokemon(objetoPokemon) {
     return async function (dispatch) {
@@ -30,13 +38,13 @@ export function postPokemon(objetoPokemon) {
     }
 }
 
-export function searchPokemon(texto){
-    return function(dispatch) {
-        return fetch(`${baseURL}/pokemons?name=${texto}`)
-        .then(data => data.json())
-        .then(respuesta => dispatch({type: SEARCH_POKEMON, payload:respuesta}))
-    }
-}
+// export function searchPokemon(texto){
+//     return function(dispatch) {
+//         return fetch(`${baseURL}/pokemons?name=${texto}`)
+//         .then(data => data.json())
+//         .then(respuesta => dispatch({type: SEARCH_POKEMON, payload:respuesta}))
+//     }
+// }
 
 export function searchPokemonFront(texto) {
     return {type:SEARCH_POKEMON_FRONT, 
@@ -78,14 +86,14 @@ export function resetFilters() {
     }
 }
 
-export function montarDetalles(id){
-    return function(dispatch) {
-        return fetch(`${baseURL}/pokemons/${id}`).then(data => data.json())
-        .then(objeto => {
-            dispatch({type:POKEMON_DETAILS, payload:objeto})
-        })
-    }
-}
+// export function montarDetalles(id){
+//     return function(dispatch) {
+//         return fetch(`${baseURL}/pokemons/${id}`).then(data => data.json())
+//         .then(objeto => {
+//             dispatch({type:POKEMON_DETAILS, payload:objeto})
+//         })
+//     }
+// }
 
 export function borrarDetalles() {
     return { type: BORRAR_DETALLES, payload:'hi'}
